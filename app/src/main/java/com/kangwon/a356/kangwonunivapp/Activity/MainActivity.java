18779b28b8e892236dfd52019d5b82667bc9e985
Rohjihyun <latsue@naver.com>
2018-10-17 22:47:35 +0900

    add dummy files for pakage.

--- app/src/main/java/com/kangwon/a356/kangwonunivapp/Activity/MainActivity.java
+++ app/src/main/java/com/kangwon/a356/kangwonunivapp/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kangwon.a356.kangwonunivapp.Activity;
+package com.kangwon.a356.kangwonunivapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/com/kangwon/a356/kangwonunivapp/Activity/timetableActivity.java
+++ app/src/main/java/com/kangwon/a356/kangwonunivapp/Activity/timetableActivity.java
@@ -1,4 +1,4 @@
-package com.kangwon.a356.kangwonunivapp.Activity;
+package com.kangwon.a356.kangwonunivapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
