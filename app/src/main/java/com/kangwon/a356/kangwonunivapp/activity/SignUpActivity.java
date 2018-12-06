5e1d454896a0e95eadadaee78c477b21911dbd9f
HoSeokJ <43873694+HoSeokJ@users.noreply.github.com>
2018-12-06 20:18:51 +0900

    Add SingUpActivity

---
+++ app/src/main/java/com/kangwon/a356/kangwonunivapp/activity/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.kangwon.a356.kangwonunivapp.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kangwon.a356.kangwonunivapp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
