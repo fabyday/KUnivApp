5240c437e42514448020955898ef9dfd7c015328
HoSeokJ <43873694+HoSeokJ@users.noreply.github.com>
2018-12-06 20:23:29 +0900

    Fix SingUpActivity(1)

--- app/src/main/java/com/kangwon/a356/kangwonunivapp/activity/SignUpActivity.java
+++ app/src/main/java/com/kangwon/a356/kangwonunivapp/activity/SignUpActivity.java
@@ -2,6 +2,8 @@ package com.kangwon.a356.kangwonunivapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.kangwon.a356.kangwonunivapp.R;
 
@@ -10,5 +12,12 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_layout);
+
+        EditText signUpName = (EditText)findViewById(R.id.signUpName);
+        EditText signUpID = (EditText)findViewById(R.id.signUpID);
+        EditText signUpPW = (EditText)findViewById(R.id.signUpPW);
+        Button signUpButton = (Button)findViewById(R.id.signUpButton);
+
+        
     }
 }
